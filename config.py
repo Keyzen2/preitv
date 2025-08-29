e35e05dd95a33278b38c29389d6f3ebcdb11b519
Keyzen2 <keyzen@protonmail.com>
2025-08-29 10:43:55 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,29 @@
 APP_TITLE = "Buscador de Vehículos"
 APP_ICON = "🚗"
 
+# Lista de marcas con presencia comercial en Europa
 EUROPEAN_MAKES = [
-    "Audi", "BMW", "Citroen", "Dacia", "Fiat", "Ford",
-    "Mercedes-Benz", "Opel", "Peugeot", "Renault",
-    "SEAT", "Skoda", "Volkswagen", "Volvo", "Alfa Romeo",
-    "Mini", "Porsche", "Jaguar", "Land Rover"
-]
+    # Generalistas europeas
+    "Alfa Romeo", "Audi", "BMW", "Citroen", "Dacia", "Fiat", "Ford",
+    "Mercedes-Benz", "Opel", "Peugeot", "Renault", "SEAT", "Skoda",
+    "Volkswagen", "Volvo",
 
-# Clave de acceso Unsplash
-UNSPLASH_ACCESS_KEY = "E7eflyO73LGMD72hV1AoSt-4nrh5E-89xxWxYl2c4Cg"
+    # Premium y deportivas europeas
+    "Aston Martin", "Bentley", "Bugatti", "Ferrari", "Jaguar", "Lamborghini",
+    "Land Rover", "Lotus", "Maserati", "McLaren", "Mini", "Porsche", "Rolls-Royce",
+
+    # Otras europeas con menor volumen
+    "Cupra", "DS", "Smart",
+
+    # Japonesas con fuerte presencia en Europa
+    "Honda", "Infiniti", "Lexus", "Mazda", "Mitsubishi", "Nissan", "Subaru", "Suzuki", "Toyota",
+
+    # Coreanas
+    "Hyundai", "Kia", "Genesis",
+
+    # Americanas presentes en Europa
+    "Chevrolet", "Chrysler", "Dodge", "Jeep", "Tesla",
+
+    # Chinas y otras emergentes con distribución oficial
+    "BYD", "MG", "Polestar"
+]
