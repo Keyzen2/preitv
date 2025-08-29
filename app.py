268e2f22c1c11dd3912c70af90f81368b87048df
Keyzen2 <keyzen@protonmail.com>
2025-08-29 11:44:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ defaults = {
 }
 for k, v in defaults.items():
     if k not in st.session_state:
-        st.session_state[k = v]  # noqa
+        st.session_state[k] = v  # noqa
 
 # Título
 st.title("🚗 Buscador de Vehículos (Versión PRO)")
