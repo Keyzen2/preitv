6989e9fc9d29c477eccf96da453c698fc19c05b8
Keyzen2 <keyzen@protonmail.com>
2025-08-29 17:28:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -96,11 +96,11 @@ def render_main_app():
     # -----------------------------
     # Carga historial de usuario
     # -----------------------------
-    if st.session_state.user and not st.session_state.data_loaded:
-        historial, historial_rutas = load_user_data(str(st.session_state.user.id))
-        st.session_state.historial = historial
-        st.session_state.historial_rutas = historial_rutas
-        st.session_state.data_loaded = True
+    if st.session_state.user and not st.session_state.get("data_loaded"):
+    historial, historial_rutas = load_user_data(str(st.session_state.user.id))
+    st.session_state.historial = historial
+    st.session_state.historial_rutas = historial_rutas
+    st.session_state.data_loaded = True
 
     # -----------------------------
     # Panel usuario
